523f72494711969008882be8b31c2487dbdf67eb
nevrome <clemens@nevrome.de>
2017-08-12 18:55:06 +0200

    very basic testscript (atm for speed tests)

---
+++ playground/test_model_run.R
@@ -0,0 +1,16 @@
+library(gluesless)
+library(magrittr)
+
+load("/home/clemens/neomod/neomod_datapool/model_data/hex_graph.RData")
+load("/home/clemens/neomod/neomod_datapool/model_data/research_area_df.RData")
+load("/home/clemens/neomod/neomod_datapool/model_data/research_area_hex_df.RData")
+
+modelobj <- new(
+  "model_builder",
+  networkland_env = graphwrite(hex_graph),
+  number_iterations = 1001
+)
+
+modelobj %>%
+  run() %$%
+  idea_exp
