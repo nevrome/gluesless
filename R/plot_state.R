ebb62bcea465ee1295206c40747182c158892de9
nevrome <clemens@nevrome.de>
2017-10-04 15:57:49 +0200

    colour setting of stage plot to see the different ideas

--- R/plot_state.R
+++ R/plot_state.R
@@ -20,14 +20,14 @@ plot_state <- function(worldplot, states, state_id) {
         colour = "ideas"
       ),
       shape = 20
-    ) +
-    ggplot2::scale_color_gradient2(
-      low = "#218203",
-      mid = "#ffff00",
-      high = "#990000",
-      limits = c(0, 100),
-      midpoint = 50
-    )
+    ) #+
+    # ggplot2::scale_color_gradient2(
+    #   low = "#218203",
+    #   mid = "#ffff00",
+    #   high = "#990000",
+    #   limits = c(0, 100),
+    #   midpoint = 50
+    # )
 
   return(stateplot)
 }
