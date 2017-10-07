e8f503df0241138bc665e3e7fb9f1585cfcd8f63
nevrome <clemens@nevrome.de>
2017-10-07 14:50:50 +0200

    adjusted plot to new setup

--- R/plot_state.R
+++ R/plot_state.R
@@ -11,23 +11,27 @@ plot_state <- function(worldplot, states, state_id) {
 
   state <- states[[state_id]]
 
+  idea_num_range <- c(0, max(states[[length(states)]]$ideas))
+
   # plot
   stateplot <- worldplot +
-    ggplot2::geom_jitter(
+    ggplot2::geom_point(
       data = state,
       ggplot2::aes_string(
         x = "x", y = "y",
         colour = "ideas"
       ),
-      shape = 20
-    ) #+
-    # ggplot2::scale_color_gradient2(
-    #   low = "#218203",
-    #   mid = "#ffff00",
-    #   high = "#990000",
-    #   limits = c(0, 100),
-    #   midpoint = 50
-    # )
+      #shape = -as.hexmode("2B22"),
+      shape = 20,
+      size = 3
+    ) +
+    ggplot2::scale_color_gradient2(
+      low = "#218203",
+      mid = "#ffff00",
+      high = "#990000",
+      limits = idea_num_range,
+      midpoint = mean(idea_num_range)
+    )
 
   return(stateplot)
 }
