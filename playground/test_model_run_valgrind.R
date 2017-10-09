56f1e5be8d2d2e208171e8f8d2e340f462fc344c
nevrome <clemens@nevrome.de>
2017-10-09 21:25:16 +0200

    fighting with memory leaks

---
+++ playground/test_model_run_valgrind.R
@@ -0,0 +1,19 @@
+# R -d "valgrind --leak-check=full --show-reachable=yes" -f test_model_run.R
+
+library(gluesless)
+library(magrittr)
+
+load("/home/clemens/neomod/neomod_datapool/model_data/hex_graph.RData")
+load("/home/clemens/neomod/neomod_datapool/model_data/research_area_df.RData")
+load("/home/clemens/neomod/neomod_datapool/model_data/research_area_hex_df.RData")
+
+modelobj <- new(
+  "model_builder",
+  networkland_env = graphwrite(hex_graph),
+  number_iterations = 1000,
+  initial_idea_starting_positions = c(2824, 2825, 2826, 2773, 2774, 2775, 2712, 2713, 2714)
+)
+
+runres <- modelobj %>%
+  run() %$%
+  idea_exp
