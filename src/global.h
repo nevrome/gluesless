4e98dc246ee8d3a9c60ea78db92deff69ef94b40
nevrome <clemens@nevrome.de>
2017-10-21 16:11:12 +0200

    added index sort function

--- src/global.h
+++ src/global.h
@@ -12,3 +12,18 @@ inline int randunifrange(int start, int stop) {
 inline int get_number_of_digits (int i) {
   return i > 0 ? (int) log10 ((double) i) + 1 : 1;
 }
+
+// https://stackoverflow.com/questions/1577475/c-sorting-and-keeping-track-of-indexes
+template <typename T>
+std::vector<size_t> sort_indexes(const std::vector<T> &v) {
+
+  // initialize original index locations
+  std::vector<size_t> idx(v.size());
+  iota(idx.begin(), idx.end(), 0);
+
+  // sort indexes based on comparing values in v
+  sort(idx.begin(), idx.end(),
+       [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
+
+  return idx;
+}
