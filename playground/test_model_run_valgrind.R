e1de12ca129ab7dffca6f0549f26caf1c35fcda0
nevrome <clemens@nevrome.de>
2017-10-23 01:19:33 +0200

    updated debugging testscript

--- playground/test_model_run_valgrind.R
+++ playground/test_model_run_valgrind.R
@@ -1,4 +1,5 @@
-# R -d "valgrind --leak-check=full --show-reachable=yes" -f test_model_run.R
+# Rscript test_model_run_valgrind.R
+# R -d "valgrind --tool=memcheck --leak-check=full --show-reachable=yes" -f test_model_run_valgrind.R
 
 library(gluesless)
 library(magrittr)
@@ -6,12 +7,15 @@ library(magrittr)
 load("/home/clemens/neomod/neomod_datapool/model_data/hex_graph.RData")
 load("/home/clemens/neomod/neomod_datapool/model_data/research_area_df.RData")
 load("/home/clemens/neomod/neomod_datapool/model_data/research_area_hex_df.RData")
+load("/home/clemens/neomod/neomod_datapool/model_data/hex_graph_nodes.RData")
 
 modelobj <- new(
   "model_builder",
   networkland_env = graphwrite(hex_graph),
-  number_iterations = 10,
-  initial_idea_starting_positions = c(2824, 2825, 2826, 2773, 2774, 2775, 2712, 2713, 2714)
+  number_iterations = 100,
+  initial_idea_starting_positions = find_starting_pos(
+    nodes, 38.923622, 36.067470, 3
+  )
 )
 
 runres <- modelobj %>%
