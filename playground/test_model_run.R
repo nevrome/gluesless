7c262a57d168b81b366b300fa8132de139534de1
nevrome <clemens@nevrome.de>
2017-11-29 19:23:49 +0100

    testscript mit external setup

--- playground/test_model_run.R
+++ playground/test_model_run.R
@@ -1,5 +1,6 @@
 library(magrittr)
 
+#### load data ####
 load("../neomod_datapool/bronze_age/space_and_network/regions_graph.RData")
 load("../neomod_datapool/bronze_age/space_and_network/region_graph_egdes.RData")
 load("../neomod_datapool/bronze_age/space_and_network/region_graph_nodes.RData")
@@ -11,16 +12,31 @@ regions <- sf::st_read(
 ) %>%
   sf::st_transform(4326)
 
+#### prepare data ####
+start_situation <- nodes_info$`2500`
+
+start_situation_reduced <- start_situation %>%
+  dplyr::select(region_name, idea, proportion) %>%
+  tidyr::spread(idea, proportion) %>%
+  dplyr::arrange(match(region_name, start_situation$region_name))
+
+ideas <- colnames(start_situation_reduced)[-1]
+ideas_proportions <- start_situation_reduced[, -1] %>% as.matrix()
+
+#### prepare and run model ####
 modelobj <- new(
   "model_builder",
   networkland_env = graphwrite(regions_graph),
-  number_iterations = 2001
+  number_iterations = 2001,
+  ideas_list = ideas,
+  ideas_proportions_matrix = ideas_proportions
 )
 
 runres <- modelobj %>%
   run() %$%
   idea_exp
 
+#### plot ####
 states <- link_ideas_world(idea_exp = runres, world_graph = regions_graph)
 
 gluesless::plot_world(
@@ -30,4 +46,4 @@ gluesless::plot_world(
   nodes = nodes,
   plotedges = F
 ) %>%
-  plot_state(states = states, 100)
+  plot_state(states = states, 2001)
