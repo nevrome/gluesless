3ad3d2b9aba230875dfe92dfafd8f0fa1496666e
nevrome <clemens@nevrome.de>
2018-01-14 12:20:29 +0100

    changed plot state to only show one set of exclusive ideas

--- R/plot_state.R
+++ R/plot_state.R
@@ -11,41 +11,20 @@ plot_state <- function(worldplot, states, state_id) {
 
   state <- states[[state_id]]
 
-  state <- state %>% tidyr::spread(.data$idea, .data$proportion)
+  two_ideas <- unique(state$idea)
 
   state <- state %>%
-    dplyr::mutate(
-      "xleft" = .data$x - 1,
-      "xright" = .data$x + 1
-    )
-
-  state_burial_type <- state %>%
-    dplyr::select(
-      -.data$flat, -.data$mound
-    ) %>%
+    tidyr::spread(.data$idea, .data$proportion) %>%
     dplyr::filter(
-      !(.data$inhumation + .data$cremation == 0)
-    )
-
-  state_burial_structure <- state %>%
-    dplyr::select(
-      -.data$inhumation, -.data$cremation
-    ) %>%
-    dplyr::filter(
-      !(.data$flat + .data$mound == 0)
+      !(.data[[two_ideas[1]]] + .data[[two_ideas[2]]] == 0)
     )
 
   # plot
   stateplot <- worldplot +
     scatterpie::geom_scatterpie(
-      data = state_burial_type,
-      ggplot2::aes_string(x = "xleft", y = "y", group = "name"),
-      cols = c("inhumation", "cremation")
-    ) +
-    scatterpie::geom_scatterpie(
-      data = state_burial_structure,
-      ggplot2::aes_string(x = "xright", y = "y", group = "name"),
-      cols = c("flat", "mound")
+      data = state,
+      ggplot2::aes_string(x = "x", y = "y", group = "name"),
+      cols = two_ideas
     ) +
     ggplot2::ggtitle(paste0(state$timestep[1], "calBC"))
 
