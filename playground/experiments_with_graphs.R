71694fbea9382d833fd54ea5c56e7c8e79cdb390
nevrome <clemens@nevrome.de>
2018-02-08 18:30:20 +0100

    first ideas for systematic graph generation

--- playground/experiments_with_graphs.R
+++ playground/experiments_with_graphs.R
@@ -7,3 +7,43 @@ make_ring(30, directed = FALSE, mutual = FALSE, circular = TRUE) %>%
   toVisNetworkData %$%
   visNetwork(nodes, edges) %>%
   visNodes(size = 20)
+
+igraph::from_data_frame()
+
+number_of_humans <- 200
+
+humans <- tibble::tibble(
+  id = 1:number_of_humans,
+  age = as.integer(abs(rnorm(number_of_humans, mean = 0, sd = 30)))
+)
+
+number_of_steps <- 20
+
+number_of_humans/number_of_steps
+
+humans %<>%
+  dplyr::mutate(
+    generation_id = as.integer(ceiling(id/10)),
+    has_parent = FALSE
+  )
+
+from <- c()
+for (i in 1:number_of_humans) {
+  from <- i
+
+  current_generation_id <- humans$generation_id[humans$id == i]
+  child_generation <- humans %>%
+    dplyr::filter(
+      generation_id == current_generation + 1,
+      !has_parent
+    )
+
+  child <- child_generation$id %>% sample(size = 1)
+
+  humans$has_parent[humans$id == child] <- TRUE
+
+  humans
+  child
+}
+
+
