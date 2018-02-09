49811a27cd1779ca7a43f7eba8fa4d6f6525b2fc
nevrome <clemens@nevrome.de>
2018-02-09 15:41:46 +0100

    much more graph stuff

--- playground/experiments_with_graphs.R
+++ playground/experiments_with_graphs.R
@@ -1,38 +1,72 @@
-number_of_humans <- 100
-number_of_steps <- 10
+number_of_humans <- 50
+number_of_steps <- 5
 generation_length <- number_of_humans/number_of_steps
 
 humans <- tibble::tibble(
   human_id = 1:number_of_humans,
   age = as.integer(abs(rnorm(number_of_humans, mean = 0, sd = 30))),
-  generation_id = as.integer(ceiling(human_id/generation_length))
+  generation_id = as.integer(ceiling(human_id/generation_length)),
+  group = generation_id,
+  sex = rep(c("male", "female"), number_of_humans/2),
+  shape = ifelse(sex == "male", "triangle", "triangleDown")
 )
 
 resample <- function(x, ...) x[sample.int(length(x), ...)]
 
-create_parent_child_connection <- function(x, generation_length) {
-  parent_vector <- c()
-  child_vector <- c()
+heavy_sexing <- function(x) {
+  from <- c()
+  to <- c()
+  for (current_human in 1:nrow(x)) {
+    if (current_human %in% c(from, to)) next
+    current_generation <- x$generation_id[current_human]
+    current_sex <- x$sex[current_human]
+    potential_partners <- x$human_id[
+      x$generation_id == current_generation &
+        x$sex != current_sex &
+        !(x$human_id %in% c(from, to))
+    ]
+    if (length(potential_partners) == 0) next
+    partner <- resample(potential_partners, 1)
+    from <- append(from, current_human)
+    to <- append(to, partner)
+  }
+  tibble::tibble(from, to, label = "❤")
+}
+
+humans %>%
+  heavy_sexing() -> sexing
+
+sexing %>%
+  igraph::graph_from_data_frame(d = .) %>%
+  visNetwork::toVisNetworkData() %$%
+  visNetwork::visNetwork(nodes, edges) %>%
+  visNetwork::visNodes(size = 20)
+
+create_parent_child_connection <- function(x, generation_length, sexing) {
+  from <- c()
+  to <- c()
   for (parent in 1:nrow(x)) {
     parent_generation_id <- x$generation_id[parent]
     child_generation_id <- parent_generation_id + 1
     if (child_generation_id > max(x$generation_id)) break
-    child_pool <- x$human_id[x$generation_id == child_generation_id & !(x$human_id %in% child_vector)]
+    child_pool <- x$human_id[x$generation_id == child_generation_id & !(x$human_id %in% to)]
     if (parent %% generation_length == 0) {
       child <- child_pool
     } else {
       child <- resample(child_pool, sample(ifelse(length(child_pool) < 3, length(child_pool), 3), 1) - 1)
     }
     if (length(child) == 0) next
-    parent_vector <- append(parent_vector, rep(parent, length(child)))
-    child_vector <- append(child_vector, child)
+    from <- append(from, rep(parent, length(child)))
+    to <- append(to, child)
+    partner <- c(sexing$from[sexing$to == parent], sexing$to[sexing$from == parent])
+    from <- append(from, rep(partner, length(child)))
+    to <- append(to, child)
   }
-  tibble::tibble(from = parent_vector, to = child_vector) %>%
-    return()
+  tibble::tibble(from, to, label = "✌")
 }
 
 humans %>%
-  create_parent_child_connection(generation_length) -> hu
+  create_parent_child_connection(generation_length, sexing) -> kidding
 
 # pu <- hu %>% dplyr::filter(
 #     !is.na(parent),
@@ -42,9 +76,14 @@ humans %>%
 #     to = child
 #   )
 
-hu %>%
- igraph::graph_from_data_frame(d = .) %>%
+rbind(
+  sexing, kidding
+) -> network
+
+network %>%
+ igraph::graph_from_data_frame(d = ., vertices = humans) %>%
   visNetwork::toVisNetworkData() %$%
   visNetwork::visNetwork(nodes, edges) %>%
-  visNetwork::visNodes(size = 20) %>%
-  visNetwork::visHierarchicalLayout(direction = "UD", sortMethod = "directed")
+  visNetwork::visGroups() %>%
+  visNetwork::visNodes(size = 20) #%>%
+  #visNetwork::visHierarchicalLayout(direction = "UD", sortMethod = "directed")
