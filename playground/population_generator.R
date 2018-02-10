9e96c8e03935437fcbd7e041a03cb538bd978319
nevrome <clemens@nevrome.de>
2018-02-10 17:35:43 +0100

    improvements to population generator

--- playground/population_generator.R
+++ playground/population_generator.R
@@ -1,5 +1,5 @@
 population_size <- function(t) {
-  500
+  (cos(0.1 * t) + 2) * 500
 }
 
 population_age_distribution <- function(t) {
@@ -29,36 +29,76 @@ timesteps <- tibble::tibble(
 population_age_distribution(10)(10)
 
 generate_humans <- function(t, start_id, number, start_age = NA) {
-  tibble::tibble(
-    id = start_id:(start_id + number - 1),
-    death_age = population_age_distribution(t)(number),
-    current_age = ifelse(is.na(start_age), as.integer(runif(number, min = 0, max = death_age)), start_age),
-    dead = FALSE,
-    death_time = NA_integer_,
-    sex = population_sex_distribution(t)(number),
-    unit = population_unit_distribution(t)(number, 40)
-  ) %>% return()
+  if (number > 0) {
+    tibble::tibble(
+      id = start_id:(start_id + number - 1),
+      death_age = population_age_distribution(t)(number),
+      current_age = if (is.na(start_age)) {
+          as.integer(runif(number, min = 0, max = death_age))
+        } else {
+          as.integer(start_age)
+      },
+      dead = FALSE,
+      birth_time = t,
+      death_time = t + death_age,
+      sex = population_sex_distribution(t)(number),
+      unit = population_unit_distribution(t)(number, 40)
+    ) %>% return()
+  } else {
+    tibble::tibble(
+      id = integer(),
+      death_age = integer(),
+      current_age = integer(),
+      dead = logical(),
+      birth_time = integer(),
+      death_time =  integer(),
+      sex = character(),
+      unit = integer()
+    )
+  }
 }
+
+time <- 1:500
 
 humans <- generate_humans(0, 1, population_size(0))
 
-for (t in 1:100) {
+for (t in time) {
   humans[!humans$dead, ] %<>%
     dplyr::mutate(
       current_age = as.integer(current_age + 1),
-      dead = current_age > death_age,
-      death_time = ifelse(dead, t, NA_integer_)
+      dead = current_age >= death_age
     )
 
   alive <- nrow(humans[!humans$dead, ])
-  deaths <- sum(humans$death_time == t, na.rm = TRUE)
+  # deaths <- sum(humans$death_time == t, na.rm = TRUE)
 
-  if (deaths > 0) {
-    humans %<>% rbind(
-      generate_humans(t, max(humans$id + 1), deaths, 0)
-    )
+  if (alive >= population_size(t)) {
+    births <- 0
+  } else {
+    births <- population_size(t) - alive
   }
+
+  humans %<>% rbind(
+    generate_humans(t, max(humans$id + 1), births, 0)
+  )
 
 }
 
-nrow(humans[!humans$dead,])
+hu <- tibble::tibble(t = time) %>%
+  dplyr::mutate(
+    n = purrr::map_int(
+      t, function(x) {
+        humans %>% dplyr::filter(
+          birth_time <= x & x <= death_time
+        ) %>%
+          nrow() %>%
+          return()
+      }
+    )
+  )
+
+
+plot(time, population_size(time))
+points(hu$t, hu$n, col = "red")
+
+hist(humans$current_age[!humans$dead])
