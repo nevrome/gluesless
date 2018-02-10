f0af48dd82fd618fcd6b5a73f26d23e77da4c4af
nevrome <clemens@nevrome.de>
2018-02-10 14:57:22 +0100

    experiments with population generation

---
+++ playground/population_generator.R
@@ -0,0 +1,64 @@
+population_size <- function(t) {
+  500
+}
+
+population_age_distribution <- function(t) {
+  function(n) {
+    as.integer(abs(rnorm(n, 0, sd = 30)))
+  }
+}
+
+population_sex_distribution <- function(t) {
+  function(n) {
+    sample(c("male", "female"), n, replace = TRUE)
+  }
+}
+
+population_unit_distribution <- function(t) {
+  function(n, amount_units) {
+    sample(1:amount_units, n, replace = TRUE)
+  }
+}
+
+timesteps <- tibble::tibble(
+  t = 1:2000,
+  population_size = population_size(t),
+  population_age_distribution = I(list(population_age_distribution(t)))
+)
+
+population_age_distribution(10)(10)
+
+generate_humans <- function(t, start_id, number, start_age = NA) {
+  tibble::tibble(
+    id = start_id:(start_id + number - 1),
+    death_age = population_age_distribution(t)(number),
+    current_age = ifelse(is.na(start_age), as.integer(runif(number, min = 0, max = death_age)), start_age),
+    dead = FALSE,
+    death_time = NA_integer_,
+    sex = population_sex_distribution(t)(number),
+    unit = population_unit_distribution(t)(number, 40)
+  ) %>% return()
+}
+
+humans <- generate_humans(0, 1, population_size(0))
+
+for (t in 1:100) {
+  humans[!humans$dead, ] %<>%
+    dplyr::mutate(
+      current_age = as.integer(current_age + 1),
+      dead = current_age > death_age,
+      death_time = ifelse(dead, t, NA_integer_)
+    )
+
+  alive <- nrow(humans[!humans$dead, ])
+  deaths <- sum(humans$death_time == t, na.rm = TRUE)
+
+  if (deaths > 0) {
+    humans %<>% rbind(
+      generate_humans(t, max(humans$id + 1), deaths, 0)
+    )
+  }
+
+}
+
+nrow(humans[!humans$dead,])
