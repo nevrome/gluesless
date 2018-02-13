79f19b7d204cc500674637b09c28b8abaf4e593c
nevrome <clemens@nevrome.de>
2018-02-13 14:30:16 +0100

    added function to count living units per timestep in population

--- R/population_analysis.R
+++ R/population_analysis.R
@@ -20,3 +20,28 @@ count_living_humans_over_time <- function(humans, time) {
       )
     ) %>% return()
 }
+
+#' count living units per timestep in population
+#'
+#' @param humans tibble population
+#' @param time vector timesteps
+#'
+#' @return tibble
+#'
+#' @export
+count_living_units_over_time <- function(humans, time) {
+  tibble::tibble(time) %>%
+    dplyr::mutate(
+      n = time %>% purrr::map_int(
+        function(x) {
+          humans %>% dplyr::filter(
+            .data$birth_time <= x & x <= .data$death_time
+          ) %>%
+            magrittr::extract2("unit") %>%
+            unique %>%
+            length %>%
+            return()
+        }
+      )
+    ) %>% return()
+}
