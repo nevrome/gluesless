170d0f16c604ff55b2ea672441d86ace35e26729
nevrome <clemens@nevrome.de>
2018-02-13 11:27:55 +0100

    testing of population generator

--- playground/test_population_generation.R
+++ playground/test_population_generation.R
@@ -1,6 +1,8 @@
+timeframe <- 0:200
+
 settings <- new(
   "population_settings",
-  time = 1:200,
+  time =  timeframe,
   population_size_function = population_size,
   unit_amount_function = unit_amount,
   age_distribution_function = age_distribution,
@@ -11,5 +13,24 @@ settings <- new(
   start_unit_vector = 1:10
 )
 
-settings %>%
+test <- settings %>%
   generate_population()
+
+population_real <- test %>% count_living_humans_over_time(timeframe)
+
+population_expected <- tibble::tibble(
+  t = timeframe,
+  n = population_size(t)
+)
+
+library(ggplot2)
+test %>%
+  ggplot() +
+  geom_segment(
+    aes(y = id, yend = id, x = birth_time, xend = death_time, color = unit)
+  ) +
+  #facet_wrap(~unit) +
+  geom_vline(aes(xintercept = timeframe[1])) +
+  geom_vline(aes(xintercept = timeframe[length(timeframe)]))
+
+
