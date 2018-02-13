b5e4ebadbdb2eed7c0b14a2827506301da4dc7af
nevrome <clemens@nevrome.de>
2018-02-13 11:26:22 +0100

    added function to count living humans per timestep in population

---
+++ R/population_analysis.R
@@ -0,0 +1,22 @@
+#' count living humans per timestep in population
+#'
+#' @param humans tibble population
+#' @param time vector timesteps
+#'
+#' @return tibble
+#'
+#' @export
+count_living_humans_over_time <- function(humans, time) {
+  tibble::tibble(time) %>%
+    dplyr::mutate(
+      n = time %>% purrr::map_int(
+        function(x) {
+          humans %>% dplyr::filter(
+            birth_time <= x & x <= death_time
+          ) %>%
+            nrow() %>%
+            return()
+        }
+      )
+    ) %>% return()
+}
