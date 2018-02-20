201380d29d6543f753b31ad8934800c5e20c6ad9
nevrome <clemens@nevrome.de>
2018-02-20 22:18:32 +0100

    some more work on relations generator

--- R/relations_generator_vertical.R
+++ R/relations_generator_vertical.R
@@ -13,32 +13,53 @@ generate_vertical_relations <- function(population) {
   start_time <- c()
   end_time <- c()
 
+  population$previous_partner <- NA
+
   pb <- utils::txtProgressBar(style = 3)
   for (child in 1:nrow(population)) {
-    potential_parents <- population %>% dplyr::filter(
-      (.data$birth_time + 12) <= population$birth_time[child],
-      population$birth_time[child] <= (.data$birth_time + 60),
-      .data$unit == population$unit[child]
-    )
+
+    potential_parents <- get_potential_parents(population, child)
+
     if (!all(c("male", "female") %in% unique(potential_parents$sex))) {next}
-    mother <- potential_parents %>%
-      dplyr::filter(.data$sex == "female") %>%
-      dplyr::sample_n(1) %>%
-      magrittr::extract2("id")
-    father <- potential_parents %>%
-      dplyr::filter(.data$sex == "male") %>%
-      dplyr::sample_n(1) %>%
-      magrittr::extract2("id")
+
+    # randomly select partner1
+    partner1_df <- potential_parents %>%
+      dplyr::sample_n(1)
+
+    monogamous <- runif(1,0,1) > 0.5
+
+    if(
+      # check if partner1 already has a previous partner
+      !is.na(partner1_df$previous_partner) &&
+      # check if the previous partner is in the right age for another child
+      partner1_df$previous_partner %in% potential_parents &&
+      # check if partner1 behaves monogamous
+      monogamous
+    ) {
+      # the previous partner becomes also the parent of this child
+      partner2_df <- population[partner1_df$previous_partner, ]
+    } else {
+      # a new partner is selected
+      partner2_df <- select_new_partner(potential_parents, partner1_df)
+    }
+
+    population$previous_partner[partner1_df$id] <- partner2_df$id
+    population$previous_partner[partner2_df$id] <- partner1_df$id
+
+
+    partner1 <- partner1_df$id
+    partner2 <- partner2_df$id
+
     from <- append(
       from, c(
-        mother,
-        mother,
-        father
+        partner1,
+        partner1,
+        partner2
       )
     )
     to <- append(
       to, c(
-        father,
+        partner2,
         child,
         child
       )
@@ -73,4 +94,23 @@ generate_vertical_relations <- function(population) {
 
   return(vertical_relations)
 
+}
+
+#### helper functions ####
+
+get_potential_parents <- function(population, child) {
+  population %>%
+    dplyr::filter(
+      .data$unit == population$unit[child]
+    ) %>%
+    dplyr::filter(
+      (.data$birth_time + 12) <= population$birth_time[child],
+      population$birth_time[child] <= (.data$birth_time + 60)
+  )
+}
+
+select_new_partner <- function(potential_parents, partner1_df) {
+  potential_parents %>%
+    dplyr::filter(.data$sex != partner1_df$sex) %>%
+    dplyr::sample_n(1)
 }
