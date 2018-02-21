f04ad108fdb8ee58f22b10fe006824987bebbe10
nevrome <clemens@nevrome.de>
2018-02-21 15:43:05 +0100

    fixed package checks

--- R/relations_settings_class.R
+++ R/relations_settings_class.R
@@ -3,6 +3,11 @@
 #' class to store values that should be passed to the relations generator
 #'
 #' @slot population test
+#' @slot monogamy_probability test
+#' @slot start_fertility_age test
+#' @slot stop_fertility_age test
+#' @slot same_unit_as_child_probability test
+#' @slot same_unit_as_partner_probability test
 #'
 #' @export
 setClass(
