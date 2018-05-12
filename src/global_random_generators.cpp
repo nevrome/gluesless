5fb454ededd9764db70887bc8c6b83d0e44957aa
nevrome <clemens@nevrome.de>
2018-05-12 21:42:54 +0200

    better solution for the random generator and seed definition+declaration

---
+++ src/global_random_generators.cpp
@@ -0,0 +1,4 @@
+#include <random>
+
+std::random_device rd; 
+std::mt19937 rng(rd());
--- src/global_random_generators.h
+++ src/global_random_generators.h
@@ -1,9 +1,11 @@
+#pragma once
+
 #include <random>
 #include <algorithm>
 #include <math.h>
 
-inline std::random_device rd; // only used once to initialise (seed) engine
-inline std::mt19937 rng(rd()); // random-number engine used (Mersenne-Twister in this case)
+extern std::random_device rd; // only used once to initialise (seed) engine
+extern std::mt19937 rng; // random-number engine used (Mersenne-Twister in this case)
 
 inline int randunifrange(int start, int stop) {
   std::uniform_int_distribution<int> uni(start, stop); // guaranteed unbiased
