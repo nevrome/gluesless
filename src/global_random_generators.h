ba9e95569f52c2c8c51212ae260421713aaa0753
nevrome <clemens@nevrome.de>
2018-07-02 00:33:00 +0200

    new random number generator

--- src/global_random_generators.h
+++ src/global_random_generators.h
@@ -8,6 +8,11 @@ extern std::random_device rd; // only used once to initialise (seed) engine
 extern std::mt19937 rng; // random-number engine used (Mersenne-Twister in this case)
 
 inline int randunifrange(int start, int stop) {
-  std::uniform_int_distribution<int> uni(start, stop); // guaranteed unbiased
+  std::uniform_int_distribution<int> uni(start, stop);
+  return uni(rng);
+}
+
+inline double randunif_zero_one() {
+  std::uniform_real_distribution<double> uni(0, 1);
   return uni(rng);
 }
